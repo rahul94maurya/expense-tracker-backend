c56f47f250dc2e0b80d7bdb01b332f3e89487ec9
praveengupta-tech <praveengupta@fynd-external.com>
2026-06-27 02:18:20 +0530

    basic get and post API created along with mvc patterns

--- app.js
+++ app.js
@@ -2,22 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
-// const mongoURI =
-//   "mongodb+srv://rahul_maurya:rahul123@cluster0.yc4zzqt.mongodb.net/expense_tracker?appName=Cluster0";
+const expensesRoutes = require("./routes/expenses");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.use("/", (req, res, next) => {
-  res.json({ message: "Hello World" });
-});
+app.use("/expenses", expensesRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Connected to MongoDB");
     app.listen(process.env.PORT);
   })
   .catch((err) => {
